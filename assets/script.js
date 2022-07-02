1afa0f07175b349550fe81458be94a8d015e262b
President <aaron.m.wilson@uconn.edu>
2022-07-02 14:07:26 -0400

    added variables for api call broken down

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,10 @@
 var apiKey = "35b4263be3c8fb07e61b756e7b5c6337"
 var submitButton = document.getElementById("button");
 
+// Defining Variables for api call using user input
+var api = "http://api.openweathermap.org/geo/1.0/direct?q="
+var city = "input"
+var apiv2 = "&limit=5&appid=35b4263be3c8fb07e61b756e7b5c6337"
 
 
-// Add Event listener for submit button on webpage
-function returnText(){
-    
-    var cityName = document.getElementById("userInput").value;
-    
-    window.localStorage.setItem(cityName, JSON.stringify(cityName));
 
-};
