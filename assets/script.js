be331c8b0127f5541d1684fa427417b7c61a79fd
President <aaron.m.wilson@uconn.edu>
2022-07-02 12:48:12 -0400

    added event listener for button in html and added a function to set the text of a variable to the input value and save that input to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
+// Defining Variables from Html Elements
 var apiKey = "35b4263be3c8fb07e61b756e7b5c6337"
 var submitButton = document.getElementById("button");
-var cityName = document.getElementById("userInput");
+
+
+
+// Add Event listener for submit button on webpage
+function returnText(){
+    
+    var cityName = document.getElementById("userInput").value;
+    
+    window.localStorage.setItem(cityName, JSON.stringify(cityName));
+
+};
