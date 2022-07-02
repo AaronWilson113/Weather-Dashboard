6766a4782aef0297c2094286066f65a96da44779
President <aaron.m.wilson@uconn.edu>
2022-07-02 14:45:38 -0400

    finalized my add event listener code to call information from api when user types in input and presses button. This will allow me to get the lat and lon cords for my city and input that into my second api call

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
 // Defining Variables from Html Elements
 var apiKey = "35b4263be3c8fb07e61b756e7b5c6337"
 var submitButton = document.getElementById("button");
+var input = document.getElementById("userInput")
+
 
 // Defining Variables for api call using user input
 var apiv1 = "http://api.openweathermap.org/geo/1.0/direct?q=";
-var city = "london";
 var apiv2 = "&limit=5&appid=35b4263be3c8fb07e61b756e7b5c6337";
 
 
 
 submitButton.addEventListener("click" , function(){
 
-    var requestString = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=35b4263be3c8fb07e61b756e7b5c6337" 
+    var requestString = apiv1 + input.value + apiv2
 
     fetch(requestString)
      .then (function (response){
