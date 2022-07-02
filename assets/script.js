7092277b6e85847860e9f465299f799989c2fa49
President <aaron.m.wilson@uconn.edu>
2022-07-02 14:24:06 -0400

    just spent and 2 hours trying to figure out why my addeventlistener wasnt working only to find out it was because i put it inside a form in my html. AGH. Well it works now and I tested it

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,27 @@ var apiKey = "35b4263be3c8fb07e61b756e7b5c6337"
 var submitButton = document.getElementById("button");
 
 // Defining Variables for api call using user input
-var api = "http://api.openweathermap.org/geo/1.0/direct?q="
-var city = "input"
-var apiv2 = "&limit=5&appid=35b4263be3c8fb07e61b756e7b5c6337"
+var apiv1 = "http://api.openweathermap.org/geo/1.0/direct?q=";
+var city = "london";
+var apiv2 = "&limit=5&appid=35b4263be3c8fb07e61b756e7b5c6337";
+
+
+
+submitButton.addEventListener("click" , function(){
+
+    var requestString = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=35b4263be3c8fb07e61b756e7b5c6337" 
+
+    fetch(requestString)
+     .then (function (response){
+        return response.json();
+        
+    })
+    .then(function (data){
+        console.log(data)
+    })
+
+})
+
 
 
 
