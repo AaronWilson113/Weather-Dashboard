c9d80a7ab11c3f008a4922068d43743a012453a0
President <aaron.m.wilson@uconn.edu>
2022-07-04 17:28:31 -0400

    created for loop to render weather forecast to the page in one fell swoop

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var apiKey = "35b4263be3c8fb07e61b756e7b5c6337"
 var submitButton = document.getElementById("button");
 var input = document.getElementById("userInput")
 var firstCard = document.getElementById("data-container")
+var secondCard = document.getElementById("weather-container");
 
 // Defining Variables for first api call using user input
 var apiv1 = "http://api.openweathermap.org/geo/1.0/direct?q=";
@@ -40,7 +41,6 @@ submitButton.addEventListener("click" , function(){
  // Api Call for weather data. Passed variables in function
  function secondApiCall(lonCord , latCord) {
 
-    
     var requestString2 = weatherApiv1 + latCord + weatherApiv2 + lonCord + weatherApiv3;
 
     fetch(requestString2)
@@ -50,7 +50,7 @@ submitButton.addEventListener("click" , function(){
     })
     .then(function(data){
         firstCard.innerHTML = ""
-        
+        console.log(data)
         //creating elements and then appending data to the page for current weather
         var cityTitle = document.createElement("p");
         var temp = document.createElement("p");
@@ -70,6 +70,22 @@ submitButton.addEventListener("click" , function(){
         firstCard.appendChild(humidity);
         firstCard.appendChild(UVindex);
         
+        //creating elements and then appending data to the page for future weather
+        for (var i=0; i <5; i++){
+            console.log(data.daily[i].temp.day)
+
+            var ftemp = document.createElement("p");
+            var fwind = document.createElement("p");
+            var fhumidity = document.createElement("p");
+
+            ftemp.textContent = data.daily[i].temp.day;
+            fwind.textContent = data.daily[i].wind;
+            fhumidity.textContent = data.daily[i].humidity;
+
+            secondCard.appendChild(ftemp);
+            secondCard.appendChild(fwind);
+            secondCard.appendChild(fhumidity);
+        }
     })
 
 }
